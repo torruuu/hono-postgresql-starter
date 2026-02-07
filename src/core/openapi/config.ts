35a7e83c3ba10b05c73e74bd6f730407b207d31d
Alejandro <ale.torrubiano@gmail.com>
2026-02-07 01:51:16 +0100

    feat: scalar

--- src/core/openapi/config.ts
+++ src/core/openapi/config.ts
@@ -1,5 +1,6 @@
 import packageJSON from '@/../package.json' with { type: 'json' }
 import type { OpenAPIHono } from '@hono/zod-openapi'
+import { Scalar } from '@scalar/hono-api-reference'
 
 export default function defineOpenAPI(app: OpenAPIHono) {
   app.doc('/docs', {
@@ -9,4 +10,16 @@ export default function defineOpenAPI(app: OpenAPIHono) {
       version: packageJSON.version,
     },
   })
+
+  app.get(
+    '/reference',
+    Scalar({
+      url: '/docs',
+      theme: 'deepSpace',
+      defaultHttpClient: {
+        targetKey: 'js',
+        clientKey: 'fetch',
+      },
+    }),
+  )
 }
