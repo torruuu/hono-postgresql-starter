2162af07202b12901eb9f1d7c5ee7d725dcb3e8d
Alejandro <ale.torrubiano@gmail.com>
2026-02-25 20:04:06 +0100

    feat: http error with zod issues

--- src/features/user/index.ts
+++ src/features/user/index.ts
@@ -1,8 +1,9 @@
+import defaultHook from '@/core/openapi/default-hook.js'
 import UserController from '@/features/user/user.controller.js'
 import * as routes from '@/features/user/user.routes.js'
 import { OpenAPIHono } from '@hono/zod-openapi'
 
-const router = new OpenAPIHono()
+const router = new OpenAPIHono({ defaultHook })
   .openapi(routes.getAllUsersRoute, UserController.getAllUsers)
   .openapi(routes.getUserByIdRoute, UserController.getUserById)
 
